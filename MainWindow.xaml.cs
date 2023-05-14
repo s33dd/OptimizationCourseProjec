f1656a436b8bcbee0d27699d0fba593df0c5bc1b
s33dd <dsuhoverikov@gmail.com>
2023-05-14 17:49:06 +0200

    validation and txt generation

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,34 +21,87 @@ namespace OptimizationCourseProject {
     public partial class MainWindow : Window {
         private int initResultLength;
         private int initArgsLength;
+        private Optimization opt;
         public MainWindow() {
             InitializeComponent();
             initResultLength = Result.Content.ToString().Length;
             initArgsLength = Arguments.Content.ToString().Length;
+            opt = new Optimization();
+            this.DataContext = opt;
+            FileInfo methodsFile = new FileInfo("methods.txt ");
+            if (!methodsFile.Exists) {
+                methodsFile.Create().Close();
+                using (StreamWriter sw = methodsFile.AppendText()) {
+                    sw.WriteLine("Метод случайных направлений (мод.)");
+                    sw.WriteLine("Метод Бокса");
+                }
+            }
+            FileInfo varsFile = new FileInfo("variants.txt ");
+            if (!varsFile.Exists) {
+                varsFile.Create().Close();
+                using (StreamWriter sw = varsFile.AppendText()) {
+                    sw.WriteLine("Вариант 11");
+                }
+            }
+            List<string> methods = new List<string>();
+            List<string> vars = new List<string>();
+            using (StreamReader sr = methodsFile.OpenText()) {
+                string s;
+                while ((s = sr.ReadLine()) != null) {
+                    methods.Add(s);
+                }
+            }
+            using (StreamReader sr = varsFile.OpenText()) {
+                string s;
+                while ((s = sr.ReadLine()) != null) {
+                    vars.Add(s);
+                }
+            }
+            Method.ItemsSource = methods;
+            Variant.ItemsSource = vars;
         }
 
+        public static IEnumerable<T> FindVisualChildren<T>(DependencyObject depObj) where T : DependencyObject {
+            if (depObj == null)
+                yield return (T)Enumerable.Empty<T>();
+            for (int i = 0; i < VisualTreeHelper.GetChildrenCount(depObj); i++) {
+                DependencyObject ithChild = VisualTreeHelper.GetChild(depObj, i);
+                if (ithChild == null)
+                    continue;
+                if (ithChild is T t)
+                    yield return t;
+                foreach (T childOfChild in FindVisualChildren<T>(ithChild))
+                    yield return childOfChild;
+            }
+        }
         private void Calculate_Click(object sender, RoutedEventArgs e) {
+            foreach (TextBox tb in FindVisualChildren<TextBox>(this)) {
+                tb.GetBindingExpression(TextBox.TextProperty).UpdateSource();
+            }
             Result.Content = Result.Content.ToString().Substring(0, initResultLength);
             Arguments.Content = Arguments.Content.ToString().Substring(0, initArgsLength);
-            List<double> limits = new List<double>();
-            limits.Add(double.Parse(FirstMin.Text));
-            limits.Add(double.Parse(FirstMax.Text));
-            limits.Add(double.Parse(SecondMin.Text));
-            limits.Add(double.Parse(SecondMax.Text));
-            limits.Add(double.Parse(ThirdLimit.Text));
-            Optimization opt = new Optimization(double.Parse(X1Start.Text), double.Parse(X2Start.Text), limits, 2,
-                                                int.Parse(MaxIter.Text), (bool)Min.IsChecked, double.Parse(Epsilon.Text), 
-                                                double.Parse(DeltaPressure1.Text), double.Parse(DeltaPressure2.Text));
             try {
                 List<Point> points = opt.RandomSearch();
                 ResultTable.ItemsSource = points;
                 Point result = points.Last();
                 Result.Content += result.Value.ToString() + " у. е.";
-                Arguments.Content += $"T1 = {result.X1} °C T2 = {result.X2} °C";
+                Arguments.Content += $"T1 = {Math.Round(result.X1, 2)} °C T2 = {Math.Round(result.X2, 2)} °C";
             }
             catch (PointException exc) {
                 MessageBox.Show(exc.Message, "Ошибка");
             }
         }
+
+        private void TextBoxValidation_Error(object sender, ValidationErrorEventArgs e) {
+            foreach (TextBox tb in FindVisualChildren<TextBox>(this)) {
+                if (Validation.GetHasError(tb)) {
+                    Calculate.IsEnabled = false;
+                    return;
+                }
+            }
+            Calculate.IsEnabled = true;
+        }
+
+
     }
 }
--- Optimization.cs
+++ Optimization.cs
@@ -6,52 +6,43 @@ using System.Threading.Tasks;
 
 namespace OptimizationCourseProject {
     public class Optimization {
-        private double _x1;
-        private double _x2;
-        private double _p1;
-        private double _p2;
-        private double _firstArgMin;
-        private double _firstArgMax;
-        private double _secondArgMin;
-        private double _secondArgMax;
-        private double _thirdLimit;
-
         private const double _gamma = 3.14;
+        public double X1 { get; set; }
+        public double X2 { get; set; }
+        public double P1 { get; set; }
+        public double P2 { get; set; }
+        public double FirstArgMin { get; set; }
+        public double FirstArgMax { get; set; }
+        public double SecondArgMin { get; set; }
+        public double SecondArgMax { get; set; }
+        public double ThirdLimit { get; set; }
+        public int N { get; set; }
+        public int MaxItertions { get; set; }
+        public double MinEpsilon { get; set; }
+        public bool IsMin { get; set; }
+        public Optimization() {
+            IsMin = true;
+            P1 = 1;
+            P2 = 1;
+            FirstArgMin = -3;
+            FirstArgMax = 0;
+            SecondArgMin = -0.5;
+            SecondArgMax = 3;
+            ThirdLimit = 3;
+            N = 2;
+            MaxItertions = 20;
+            MinEpsilon = 0.01;
 
-        private int _n;
-        private int _maxIterations;
-        private bool _isMin;
-
-        private double _minEpsilon;
-        public Optimization(double x1, double x2, List<double> limits, int n, int maxIter, bool isMin, double epsilon, double p1, double p2) { 
-            _x1 = x1;
-            _x2 = x2;
-
-            _firstArgMin = limits[0];
-            _firstArgMax = limits[1];
-            _secondArgMin = limits[2];
-            _secondArgMax = limits[3];
-            _thirdLimit = limits[4];
-
-            _isMin = isMin;
-
-            _n = n; 
-            _maxIterations = maxIter;
-
-            _minEpsilon = epsilon;
-
-            _p1 = p1;
-            _p2 = p2;
         }
         public List<Point> RandomSearch() {
-            if (!PointCheck(_x1, _x2)) {
+            if (!PointCheck(X1, X2)) {
                 throw new PointException("Стартовая точка за пределами ограничений!");
             }
             List<Point> result = new List<Point>();
-            result.Add(new Point(_x1, _x2, this));
+            result.Add(new Point(Math.Round(X1, 3), Math.Round(X2, 3), this));
             int count = 0;
             double epsilon = 1;
-            while (count < _maxIterations) {
+            while (count < MaxItertions) {
                 count++;
                 double[] alpha;
                 double step = 1.0;
@@ -59,33 +50,33 @@ namespace OptimizationCourseProject {
                 double nextX2;
 
                 for (step /= 1; step > 0.1; step /= 2) {
-                    for (int i = 0; i < _n; i++) {
+                    for (int i = 0; i < N; i++) {
                         alpha = NewRandomVector();
-                        nextX1 = _x1 + step * alpha[0];
-                        nextX2 = _x2 + step * alpha[1];
+                        nextX1 = X1 + step * alpha[0];
+                        nextX2 = X2 + step * alpha[1];
                         if (!PointCheck(nextX1, nextX2)) {
                             continue;
                         }
-                        switch (_isMin) {
+                        switch (IsMin) {
                             case true: {
-                                    double prevValue = FunctionValue(_x1, _x2);
+                                    double prevValue = FunctionValue(X1, X2);
                                     double nextValue = FunctionValue(nextX1, nextX2);
                                     if (nextValue < prevValue ) {
-                                        _x1 = nextX1;
-                                        _x2 = nextX2;
+                                        X1 = nextX1;
+                                        X2 = nextX2;
                                         result.Add(new Point(Math.Round(nextX1, 3), Math.Round(nextX2, 3), this));
-                                        epsilon = Math.Abs(FunctionValue(nextX1, nextX2) - FunctionValue(_x1, _x2));
+                                        epsilon = Math.Abs(FunctionValue(nextX1, nextX2) - FunctionValue(X1, X2));
                                         i = 0;
                                         step = 1.0;
                                     }
                                     break;
                                 }
                             case false: {
-                                    if (FunctionValue(nextX1, nextX2) > FunctionValue(_x1, _x2)) {
-                                        _x1 = nextX1;
-                                        _x2 = nextX2;
+                                    if (FunctionValue(nextX1, nextX2) > FunctionValue(X1, X2)) {
+                                        X1 = nextX1;
+                                        X2 = nextX2;
                                         result.Add(new Point(Math.Round(nextX1, 3), Math.Round(nextX2, 3), this));
-                                        epsilon = Math.Abs(FunctionValue(nextX1, nextX2) - FunctionValue(_x1, _x2));
+                                        epsilon = Math.Abs(FunctionValue(nextX1, nextX2) - FunctionValue(X1, X2));
                                         i = 0;
                                         step = 1.0;
                                     }
@@ -95,14 +86,14 @@ namespace OptimizationCourseProject {
                         }
                     }
                 }
-                if (epsilon < _minEpsilon) {
+                if (epsilon < MinEpsilon) {
                     break;
                 }
             }
             return result;
         }
         private bool PointCheck(double x1, double x2) {
-            if (x1 < _firstArgMin || x1 > _firstArgMax || x2 < _secondArgMin || x2 > _secondArgMax || x2 - x1 > _thirdLimit) {
+            if (x1 < FirstArgMin || x1 > FirstArgMax || x2 < SecondArgMin || x2 > SecondArgMax || x2 - x1 > ThirdLimit) {
                 return false;
             } else {
                 return true;
@@ -111,22 +102,22 @@ namespace OptimizationCourseProject {
         private double[] NewRandomVector() {
             const double upperBound = 1;
             const double lowerBound = -1;
-            double[] beta = new double[_n];
-            double[] alpha = new double[_n];
+            double[] beta = new double[N];
+            double[] alpha = new double[N];
             double betaSquareSum = 0;
-            for (int i = 0; i < _n; i++) {
+            for (int i = 0; i < N; i++) {
                 Random rand = new Random();
                 beta[i] = rand.NextDouble() * (upperBound - lowerBound) + lowerBound;
                 betaSquareSum += Math.Pow(beta[i], 2);
             }
-            for (int i = 0; i < _n; i++) {
+            for (int i = 0; i < N; i++) {
                 alpha[i] = beta[i] / Math.Sqrt(betaSquareSum);
             }
             return alpha;
         }
         public double FunctionValue(double x1, double x2) {
-            double rads = _gamma * _p2 * Math.Sqrt(Math.Pow(x1, 2) + Math.Pow(x2, 2)) * Math.PI / 180;
-            double value = Math.Abs(800 * (x1 - _p1) * Math.Cos(rads));
+            double rads = _gamma * P2 * Math.Sqrt(Math.Pow(x1, 2) + Math.Pow(x2, 2)) * Math.PI / 180;
+            double value = Math.Abs(800 * (x1 - P1) * Math.Cos(rads));
             return Math.Round(value, 3);
         }
     }
