da1a72d0ced765c600e6135d5e9eaf2099526c62
s33dd <dsuhoverikov@gmail.com>
2023-05-14 18:16:56 +0200

    removed unnecessary usings

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -2,17 +2,9 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace OptimizationCourseProject {
     /// <summary>
@@ -111,7 +103,5 @@ namespace OptimizationCourseProject {
             }
             Calculate.IsEnabled = true;
         }
-
-
     }
 }
